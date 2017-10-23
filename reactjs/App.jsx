3898bb31dc3c7d8d40f2b00c2aaa3751b88402f7
Darren Surtie <darren@comprsa.com>
2017-10-23 18:04:35 +0200

    reactjs Tutorials

--- reactjs/App.jsx
+++ reactjs/App.jsx
@@ -1,1 +1,13 @@
-ÿþ
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            Hello World!!!
+         </div>
+      );
+   }
+}
+
+export default App;
--- reactjs/main.js
+++ reactjs/main.js
@@ -1,1 +1,5 @@
-ÿþ
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));

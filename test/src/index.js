438a5bf4f7628daf04419feff5cbef1e74b0681e
Darren Surtie <darren@comprsa.com>
2017-10-23 15:30:53 +0200

    Learning React

--- test/src/index.js
+++ test/src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-var element = React.createElement('h1', { classname: 'greeting' }, 'Hello, World!');
+var element = React.createElement('h1', { className: 'greeting' }, 'Hello, World!');
 ReactDOM.render(element, document.getElementById('root'));
 registerServiceWorker();

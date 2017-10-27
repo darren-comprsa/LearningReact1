4e9ee61bad167053129dfe7f91b4ba27b176ff18
Darren Surtie <darren@comprsa.com>
2017-10-27 14:57:49 +0200

    test

--- reactjs/App.jsx
+++ reactjs/App.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import ReactDOM from 'react-dom';
-import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
 
 class App extends React.Component {
-    constructor() {
-        super();
-    }
-
-    render() {
-        return (
-        <div>
-            
-        </div>
+   render() {
+      return (
+         <div>
+            Hello World!!!
+         </div>
       );
    }
 }
